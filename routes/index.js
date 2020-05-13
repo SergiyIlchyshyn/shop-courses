9bf235c7c51fc60977726a5548b21aa5e3f40a40
SergiyIlchyshyn <sergiy.ilchyshyn@gmail.com>
2020-05-13 22:16:00 +0300

    Data rendering

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-    res.render('index', { title: 'Home paige' });
+    res.render('index', {
+        title: 'Home page',
+        isHome: true
+    });
+});
+/* GET courses page. */
+router.get('/courses', (req, res, next) => {
+    res.render('courses', {
+        title: 'Courses page',
+        isCourses: true
+    });
+});
+/* GET add page. */
+router.get('/add', (req, res, next) => {
+    res.render('add', {
+        title: 'Add course',
+        isAdd: true
+    });
 });
 
 module.exports = router;
