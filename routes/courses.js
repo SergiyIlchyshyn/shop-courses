437cbd0b25d54e7164aff3496a02f45ac5997289
SergiyIlchyshyn <sergiy.ilchyshyn@gmail.com>
2020-05-15 11:58:35 +0300

    Listing Courses

--- routes/courses.js
+++ routes/courses.js
@@ -1,10 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-router.get('/', (req, res, next) => {
+const Course = require('../models/course');
+
+router.get('/', async(req, res, next) => {
+    const courses = await Course.getAll();
+    console.log(courses);
     res.render('courses', {
         title: 'Courses page',
-        isCourses: true
+        isCourses: true,
+        courses
     });
 });
 
