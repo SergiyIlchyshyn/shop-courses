d601f3cea44500bd68a91fcc7318f602f86f8ff9
SergiyIlchyshyn <sergiy.ilchyshyn@gmail.com>
2020-05-16 14:57:43 +0300

    Dynamic parameters

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,3 +1,11 @@
-$(document).ready(function() {
-    $('.sidenav').sidenav();
-});
+window.onload = function() {
+    let elem = document.querySelector('.sidenav');
+    let instance = new M.Sidenav(elem);
+    // ===========================================
+    document.querySelectorAll('.price').forEach(node => {
+        node.textContent = new Intl.NumberFormat('us-US', {
+            currency: 'USD',
+            style: 'currency'
+        }).format(node.textContent);
+    })
+}
--- routes/courses.js
+++ routes/courses.js
@@ -5,12 +5,21 @@ const Course = require('../models/course');
 
 router.get('/', async(req, res, next) => {
     const courses = await Course.getAll();
-    console.log(courses);
+    // console.log(courses);
     res.render('courses', {
         title: 'Courses page',
         isCourses: true,
         courses
     });
 });
+
+router.get('/:id', async(req, res) => {
+    const course = await Course.getById(req.params.id);
+    res.render('course', {
+        layout: 'empty',
+        title: `Course ${course.title}`,
+        course
+    });
+})
 
 module.exports = router;
