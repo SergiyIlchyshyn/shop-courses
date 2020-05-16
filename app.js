dbd880f84c9ee791cfbfb69db4d826e35c8da690
SergiyIlchyshyn <sergiy.ilchyshyn@gmail.com>
2020-05-16 19:57:50 +0300

    Card model

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var coursesRouter = require('./routes/courses');
 var addRouter = require('./routes/add');
+var cardRouter = require('./routes/card');
 //==============================================================================
 var app = express();
 //HANDLEBARS====================================================================
@@ -32,6 +33,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/courses', coursesRouter);
 app.use('/add', addRouter);
+app.use('/card', cardRouter);
 //==============================================================================
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
