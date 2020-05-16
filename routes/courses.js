62435f00161e28416d6554bac94d0ac8b353ac15
SergiyIlchyshyn <sergiy.ilchyshyn@gmail.com>
2020-05-16 15:20:20 +0300

    Course Editing

--- routes/courses.js
+++ routes/courses.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const Course = require('../models/course');
-
+// Get all courses
 router.get('/', async(req, res, next) => {
     const courses = await Course.getAll();
     // console.log(courses);
@@ -12,7 +12,7 @@ router.get('/', async(req, res, next) => {
         courses
     });
 });
-
+// Get one course by ID
 router.get('/:id', async(req, res) => {
     const course = await Course.getById(req.params.id);
     res.render('course', {
@@ -20,6 +20,24 @@ router.get('/:id', async(req, res) => {
         title: `Course ${course.title}`,
         course
     });
-})
+});
+// Edit course
+router.get('/:id/edit', async(req, res) => {
+    if (!req.query.allow) {
+        return res.redirect('/');
+    }
+
+    const course = await Course.getById(req.params.id);
+
+    res.render('course-edit', {
+        title: `Edit course - ${course.title}`,
+        course
+    })
+});
+router.post('/edit', async(req, res) => {
+    await Course.update(req.body);
+
+    res.redirect('/courses');
+});
 
 module.exports = router;
